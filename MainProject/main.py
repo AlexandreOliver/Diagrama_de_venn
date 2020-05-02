23eb3d6c41e3edd0e2b5e96f1ce7cb04596b0b1d
Richard Smanhoto <richard.11smanhoto@gmail.com>
2020-05-02 10:20:34 -0300

    fix bug import

---
+++ MainProject/main.py
@@ -0,0 +1,27 @@
+from time import sleep
+from Connect.connection import options
+
+print("-=" * 20)
+print("PROGRAMA DE RESOLUÇÃO DO DIAGRAMA DE VENN")
+print("-=" * 20)
+num = 4
+while num > 0:
+    valores = [input("Conjunto A: "),
+               input("Conjunto B: "),
+               input("Intersecção: "),
+               input("Total: ")]    #Conjunto U
+    sleep(1)
+    for value in valores:
+        if value.isnumeric():
+            value = valores.index(value)
+            valores[value] = int(valores[value])
+            num -= 1
+        else:
+            print("\nUtilize apenas Numeros!!\n Tente Novamente.")
+print("\n")
+print("<"*4, "O que vc deseja que o Programa faça?", ">"*4)
+print("N°", " "*15, "Opções")
+for n, opcao in options.items():
+    print(n, " "*2, opcao, "|")
+print("<"*7, "Digite o Numero Correspondente:", ">"*6)
+user = input("===> ")
