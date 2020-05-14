55414433887fb55fcfbff4e3c30fc6033455b71c
Richard Cauê Tavares Smanhoto <richard.11smanhoto@gmail.com>
2020-05-14 00:25:21 -0300

    Update firstSolutions.py

--- Project/firstSolutions.py
+++ Project/firstSolutions.py
@@ -7,28 +7,24 @@ def ConjuntoB(valores):
     return valores
 
 
-def Intersec(valores):
-    v = valores
+def Intersec(v):
     soma = v[0] + v[1] + v[3]
     solution = soma - v[4]
     return solution
 
 
-def conjInter(valores):
-    v = valores
+def conjInter(v):
     volta = (v[0] - v[2]) + (v[1] - v[2]) + v[2]
     return volta
 
 
-def ValueNotIn(valores):
-    v = valores
+def ValueNotIn(v):
     solution = conjInter(v)
     solution = (solution - v[4])*(-1)
     return solution
 
 
-def ValorTotal(valores):
-    v = valores
+def ValorTotal(v):
     solution = conjInter(v)
     solution += v[3]
     return solution
